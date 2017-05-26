52bdb3c7d9d443e46f483b477c62e902815c4251
arpitmathur89 <arpitmathur0789@gmail.com>
2017-05-26 14:14:32 -0700

    Max product subarray

--- Interview/src/array/MaximumProductSubArray.java
+++ Interview/src/array/MaximumProductSubArray.java
@@ -10,21 +10,27 @@ package array;
 public class MaximumProductSubArray {
 
 	public static void main(String[] args) {
-		int[] nums = {-2,3,-2,4,-20}
+		int[] nums = {-2,-3,-2,4,20};
 		System.out.println(maxProduct(nums));
 
 	}
 	
 	public static int maxProduct(int[] nums) {
-		if(nums.length<=0)
-			return 0;
-		int maxsofar = nums[0];
-		int max = nums[0];
-		for(int i=1;i<nums.length;i++){
-				maxsofar = Math.max(maxsofar, maxsofar*nums[i]);
-				max = Math.max(max, maxsofar);
-			}
-		return max;
+	    if(nums.length == 0)
+            return 0;
+        int posprod = nums[0];
+        int negprod = nums[0];
+        int maxprod = nums[0];
+        
+        for(int i=1;i<nums.length;i++){
+            int a = posprod * nums[i];
+            int b = negprod * nums[i];
+            
+            posprod = Math.max(Math.max(a,b),nums[i]);
+            negprod = Math.min(Math.min(a,b),nums[i]);
+            maxprod = Math.max(maxprod,posprod);
+        }
+        return maxprod;
 	}
 
 
